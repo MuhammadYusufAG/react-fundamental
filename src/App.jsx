8331fbce1f301b91c1a90c92dea539f1ef23c900
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-10-26 14:18:55 +0700

    feat : menambahkan isNew dijson dan manambahkan conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import "./App.css"
 import Home from "./pages/Index"
 
 function App() {
-  const value = false;
-
   return (
     <>
     <div className="App">
-      {value ? 'benar' : 'salah'} 
       <Home/>
     </div>
     </>
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,22 @@
-function Article (props){
+const ArticleStatus = ({ isNew }) => {
+    return isNew && <span>--Baru !!!</span>;
+};
+
+const NewArticle = () => {
+    return <span>--Baru !!</span>;
+};
+
+function Article(props) {
     return (
-        <>
-        <h3>{props.title}</h3>
-        <small>
-            Date : {props.date}, Tags : {props.tags.join(", ")}
-        </small>
-        </>
+    <>
+    <h3>{props.title}</h3>
+    <small>
+        Date : {props.date}, Tags : {props.tags.join(", ")}{" "}
+        <ArticleStatus isNew={props.isNew} />
+        {props.isNew && <NewArticle/>}
+    </small>
+    </>
     );
-} 
+}
 
 export default Article;
