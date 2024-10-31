3ff37da3d5987073888d483c81d769222dfd6f07
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-10-31 23:03:08 +0700

    feat : Create install react router and make folder routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import "./App.css"
-import Home from "./pages/Index"
+import "./App.css";
 import { GlobalContext } from "./context";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routers";
 
 function App() {
   const user = {
-    username : "agus"
-  }
+    username: "agus",
+  };
   return (
     <>
-    <div className="App">
-      <GlobalContext.Provider value={user}>
-        <Home/>
-      </GlobalContext.Provider>
-    </div>
+      <div className="App">
+        <GlobalContext.Provider value={user}>
+          <RouterProvider router={router} />
+        </GlobalContext.Provider>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
