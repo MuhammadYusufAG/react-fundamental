c0df338cba19515478d99fc27412c94b7555b7f1
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-10-31 22:32:38 +0700

    feat : create basic state management use useContext. NOTE : still hard code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css"
 import Home from "./pages/Index"
+import { GlobalContext } from "./context";
 
 function App() {
+  const user = {
+    username : "agus"
+  }
   return (
     <>
     <div className="App">
-      <Home/>
+      <GlobalContext.Provider value={user}>
+        <Home/>
+      </GlobalContext.Provider>
     </div>
     </>
   )
