e3c4641accc29ac2642a7a645a7dd99b595bdd68
Muhammad Yusuf A G <ghanyyusuf33@gmail.com>
2024-11-01 19:06:23 +0700

    feat : Create layouting and use children

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,14 +1,21 @@
 import {createBrowserRouter} from "react-router-dom"
+import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Index";
 import About from "../pages/About";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <RootLayout/>,
+    children:[
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/about",
+        element: <About/>
+      }
+    ]
   },
-  {
-    path: "/about",
-    element:<About/>
-  }
 ]);
